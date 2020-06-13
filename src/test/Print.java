e284889fbbedaa4f0e29fcffe8c026cfc353aff3
986224568 <986224568@qq.com>
2020-06-13 15:58:04 +0800

    finish

---
+++ src/test/Print.java
@@ -0,0 +1,9 @@
+package test;
+import oop.Student;
+
+public class Print {
+    public static void main(String[] args) {
+        Student lmh = new Student("lmh", 22, "男", "4406811998........");
+        System.out.println(lmh.toString());
+    }
+}
